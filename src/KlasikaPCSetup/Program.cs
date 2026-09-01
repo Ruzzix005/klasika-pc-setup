36a3f28ba0da0433ba51acd039e14069e25d201d
Ruzzix005 <121766349+Ruzzix005@users.noreply.github.com>
2026-09-01 10:53:24 +0200

    Dodaj src/KlasikaPCSetup/Program.cs

---
+++ src/KlasikaPCSetup/Program.cs
@@ -0,0 +1,11 @@
+namespace KlasikaPCSetup;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
